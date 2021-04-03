f63676bf2b70d556e5fd0699dbf872fdfb287a17
Minoplhy <zebr@m21.kylz.nl>
2021-04-03 09:44:12 +0000

    Nope

--- old.py
+++ old.py
@@ -129,3 +129,26 @@ print(polly[0])
 print(polly[0:2])
 print("Mo" in polly)
 print(len(polly))
+
+# dict = {key:vaule, key:vaule}
+actions = {"r":1, "l":-1}
+print(actions)
+
+print(actions["r"])
+print(actions.get("g"))
+
+actions["r"] = 2
+actions["h"] = 5
+print(actions)
+
+print(actions.keys())
+print(actions.values())
+print(actions.items())
+
+del(actions["r"])
+print(actions)
+
+actions.pop("h")
+print(actions)
+
+print("l" in actions)
